4b265b74c6a1982de266e66443c80d3f57be6e17
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-06-06 01:48:55 -0600

    Added top menu bar

--- src/components/MainPage/Hero.js
+++ src/components/MainPage/Hero.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './style/Hero.scss';
+import { Navbar } from './Navbar';
 
 export const Hero = () => {
   return (
-    <div>
-      {/* <img
+    <div className='hero-container'>
+      <img
         className='backgroundImage'
         src='images/desktop-large.webp'
         alt='default'
-      /> */}
-      <button className='find'>
-        <span className='findText'>Find Restaurants In Your Area</span>
-        <span></span>
-      </button>
+      />
+      <Navbar />
+      <div className='searchAddress'>
+        <div>EAT. PLAYOFFS. REPEAT</div>
+        <button className='find'>
+          <span className='findText'>Find Restaurants In Your Area</span>
+          <span></span>
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Hero } from '../components/MainPage/Hero';
-import { Navbar } from '../components/Navbar/Navbar';
 
 export const MainPage = () => {
   return (
     <div>
-      <Navbar />
       <Hero />
     </div>
   );
