71c81c27bb7ec522726971a2d6d0aca158139060
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-06-08 02:59:15 -0600

    Redesign benefit page

--- src/components/MainPage/Benefits.js
+++ src/components/MainPage/Benefits.js
@@ -4,16 +4,13 @@ import './style/Benefits.scss';
 export const Benefits = () => {
   return (
     <div className='benefits-container'>
-      <div>Benefits</div>
-      <div class='c5179 c5187'>
-        <div class='c5181'>
-          <div class='c5180 c5188'>
-            <div class='c5182'>
-              <h2 class='c5183 c5189'>More Choice</h2>
-              <div class='c5184 c5190'>
-                We have thousands of restaurants, including local favorites that
-                don't normally deliver. Discover new cuisines all around you.
-              </div>
+      <div>
+        <div className='text1'>
+          <div>
+            <h2>More Choice</h2>
+            <div>
+              We have thousands of restaurants, including local favorites that
+              don't normally deliver. Discover new cuisines all around you.
             </div>
           </div>
         </div>
@@ -21,7 +18,7 @@ export const Benefits = () => {
           className='slide1'
           style={{ backgroundImage: `url('/images/more-choice_desktop.jpg')` }}
         >
-          <div class='c5186 c5192'></div>
+          <div className='diagonal'></div>
         </div>
       </div>
     </div>
