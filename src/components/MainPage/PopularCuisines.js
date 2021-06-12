f9f6b10712937fb43425e90896178d57c1bb693b
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-06-12 01:29:50 -0600

    Completed popular cusines page

--- src/components/MainPage/PopularCuisines.js
+++ src/components/MainPage/PopularCuisines.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import './style/PopularCuisines.scss';
+import cuisines from './JSON/cuisines.json';
 
 export const PopularCuisines = () => {
-  return <div></div>;
+  return (
+    <div className='popularcuisines-container'>
+      <div>
+        <h6>Popular Cuisines</h6>
+        <ul>
+          {cuisines.map((cuisine) => (
+            <li>
+              <a href={cuisine.link}>
+                <div
+                  style={{
+                    backgroundImage: `url('/images/cuisines/${cuisine.name}.jpg')`,
+                  }}
+                >
+                  <p>{cuisine.name}</p>
+                  <div></div>
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
