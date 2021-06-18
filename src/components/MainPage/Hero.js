fe76529416328d98de41a060c113b9ecd181074c
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-06-18 00:11:09 -0600

    Added mobile logo

--- src/components/MainPage/Hero.js
+++ src/components/MainPage/Hero.js
@@ -5,11 +5,11 @@ import { Navbar } from './Navbar';
 export const Hero = () => {
   return (
     <div className='hero-container'>
-      {/* <Navbar /> */}
+      <Navbar />
       <div className='centerMenu'>
         <h1>EAT. PLAYOFFS. REPEAT</h1>
-        <div className='searchMenu'>
-          <div className='searchIcon'>
+        <form>
+          <div>
             <svg viewBox='0 0 24 24'>
               <g transform='translate(2 1.5)'>
                 <path d='m9.738 3.7341c-1.6134 0-2.9214 1.3079-2.9214 2.9214s1.308 2.9215 2.9214 2.9215c1.6135 0 2.9214-1.308 2.9214-2.9215s-1.3079-2.9214-2.9214-2.9214'></path>
@@ -23,9 +23,7 @@ export const Hero = () => {
               </g>
             </svg>
           </div>
-          <form>
-            <input placeholder='Enter Your Address' />
-          </form>
+          <input placeholder='Enter Your Address' />
           <button>
             <svg
               focusable='false'
@@ -40,12 +38,8 @@ export const Hero = () => {
               <path d='M12,8c-2.21,0-4,1.79-4,4s1.79,4,4,4s4-1.79,4-4S14.21,8,12,8z M20.94,11C20.48,6.83,17.17,3.52,13,3.06V2c0-0.55-0.45-1-1-1h0c-0.55,0-1,0.45-1,1v1.06C6.83,3.52,3.52,6.83,3.06,11H2c-0.55,0-1,0.45-1,1v0c0,0.55,0.45,1,1,1h1.06c0.46,4.17,3.77,7.48,7.94,7.94V22c0,0.55,0.45,1,1,1h0c0.55,0,1-0.45,1-1v-1.06c4.17-0.46,7.48-3.77,7.94-7.94H22c0.55,0,1-0.45,1-1v0c0-0.55-0.45-1-1-1H20.94z M12,19c-3.87,0-7-3.13-7-7s3.13-7,7-7s7,3.13,7,7S15.87,19,12,19z'></path>
             </svg>
           </button>
-        </div>
-        <div className='findRestaurantsInYourArea'>
-          <button>
-            <span>Find Restaurants In Your Area</span>
-          </button>
-        </div>
+        </form>
+        <button>Find Restaurants In Your Area</button>
       </div>
       <img
         className='backgroundImage'
