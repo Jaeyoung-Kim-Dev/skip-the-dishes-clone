9e05d0c148b3c45d15f239cd97ec2aaa10188bfa
Jaeyoung Kim <50346571+Jaeyoung-Kim-Dev@users.noreply.github.com>
2021-06-18 03:53:06 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Benefits } from '../components/MainPage/Benefits';
 import { Cards } from '../components/MainPage/Cards';
 import { DownloadOurApp } from '../components/MainPage/DownloadOurApp';
-import { Footer } from '../components/MainPage/Footer';
+// import { Footer } from '../components/MainPage/Footer';
 import { Hero } from '../components/MainPage/Hero';
 import { NowServing } from '../components/MainPage/NowServing';
 import { PopularCuisines } from '../components/MainPage/PopularCuisines';
