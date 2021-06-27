74c6274a2b58e2aa7ad350eb0b8af8bf6b058ac2
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-06-27 02:04:33 -0600

    Complete login modal design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import { MainPage } from './pages/index';
 
 function App() {
   return (
     <div className='App'>
-      <MainPage />
+      <Router>
+        <Route path='/' component={MainPage} exact />
+        {/* <Route path='/user/account/create' component={SignupPage} exact /> */}
+        {/* <Route component={NotFoundPage} /> */}
+      </Router>
     </div>
   );
 }
